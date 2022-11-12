4127044cbdbfa961cde06316a67fa3164a75b154
Mohamed Abdulrahman <mohamed.abdulrahman@kcl.ac.uk>
2022-11-12 00:50:47 +0000

    edit views function

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
+from .models import Thing
+from django import forms
 
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = {'name', 'description', 'quantity'}
+        widgets = {'description': forms.Textarea()}
+        widgets = {'quantity': forms.NumberInput()}
+        
